6f20e1067dabd2e6dc1066d2415ace1d845c0412
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-08 19:22:34 +0200

    train

--- js/task-1.js
+++ js/task-1.js
@@ -3,3 +3,19 @@
 Виконуй це завдання у файлі task-1.js
 
 */
+
+function greet(name) {
+  console.log(`Welcome ${name}!`);
+}
+
+function notify(name) {
+  console.log(`Hello ${name}!`);
+}
+
+function registerGuest(name, callback) {
+  console.log(`Registering ${name}!`);
+  callback(name);
+}
+
+registerGuest('Mango', greet);
+registerGuest('Mango', notify);
